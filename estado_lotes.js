925a3266f866b3f21027aa7dae4685a1031b8d4d
DIEGOCHT <164044510+DIEGOCHT@users.noreply.github.com>
2026-07-03 23:02:53 -0500

    Update estado_lotes.js

--- estado_lotes.js
+++ estado_lotes.js
@@ -1,47 +1,39 @@
-alert("1. Archivo cargado");
+(function () {
 
-(function(){
+    function esperarTour() {
 
-    alert("2. Entró a la función");
+        if (typeof window.tour === "undefined") {
 
-    console.log("2. Entró a la función");
+            console.log("Esperando tour...");
 
-    function iniciar(){
+            requestAnimationFrame(esperarTour);
 
-        alert("3. Entró a iniciar");
-
-        console.log("3. Entró a iniciar");
-
-        try{
-
-            alert("4. Antes de root");
-
-            const root = tour.player.wv.Xl.Xb();
-
-            alert("5. Root OK");
-
-            const pp = root.MainViewerPanoramaPlayer;
-
-            alert("6. Player OK");
-
-            const camera = pp.U.Yy.renderer.qk.getCamera();
-
-            alert("7. Cámara OK");
-
-            console.log(camera);
+            return;
 
         }
 
-        catch(e){
+        if (!tour.player) {
 
-            alert("ERROR:\n\n"+e);
+            console.log("Esperando player...");
 
-            console.error(e);
+            requestAnimationFrame(esperarTour);
+
+            return;
 
         }
+
+        alert("TOUR ENCONTRADO");
+
+        const root = tour.player.wv.Xl.Xb();
+
+        const pp = root.MainViewerPanoramaPlayer;
+
+        const camera = pp.U.Yy.renderer.qk.getCamera();
+
+        console.log(camera);
 
     }
 
-    iniciar();
+    esperarTour();
 
 })();
